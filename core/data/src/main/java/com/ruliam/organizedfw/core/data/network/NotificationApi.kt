d3311e6b5366b74933c46bd7f1dd30106f0e2b40
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2023-08-15 09:39:07 -0300

    Last changes to open repo

--- core/data/src/main/java/com/ruliam/organizedfw/core/data/network/NotificationApi.kt
+++ core/data/src/main/java/com/ruliam/organizedfw/core/data/network/NotificationApi.kt
@@ -1,5 +1,6 @@
 package com.ruliam.organizedfw.core.data.network
 
+import com.ruliam.organizedfw.core.data.BuildConfig
 import com.ruliam.organizedfw.core.data.model.PushNotification
 import okhttp3.ResponseBody
 import retrofit2.Response
@@ -10,8 +11,7 @@ import retrofit2.http.POST
 interface NotificationApi {
     companion object{
         const val CONTENT_TYPE = "application/json"
-        //TODO REMOVER A API KEY DAQ
-        const val API_KEY = "AAAAZKCLIO8:APA91bFQEgqtDPQE61E3fJ8QkhNkn8pze-04LIiqj_wdrlTuQbfMc_rfdNKtU5aC1ADUgBUt2gVVUNRve4N-zVcmIYJHlGSYvRE0e4q5YijYNC8B6lzbSw3WhOAGLqgSn7MDF6KaJ83X"
+        const val API_KEY = BuildConfig.FIREBASE_MESSAGING_API_KEY
     }
 
     @Headers("Authorization: key=${API_KEY}", "Content-Type: $CONTENT_TYPE")
